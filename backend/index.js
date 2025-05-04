4ceae962f2d87273e3208af9330068aea95b5c0e
Sathish Kumar <sk1241245@gmail.com>
2025-05-04 21:49:40 +0530

    update vite config

--- backend/index.js
+++ backend/index.js
@@ -9,11 +9,11 @@ const { updateClicks } = require("./controller/linkController");
 const app = express();
 
 app.use(cors({
-    origin: [process.env.CLIENT_URL],
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true
 }));
-app.options('*', cors());
+
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
